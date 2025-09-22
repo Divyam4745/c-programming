bbd66fc605b47bccb378243f20a9d12b37f790f0
Divyam4745 <divyamvishwakarma34@gmail.com>
2025-09-22 16:58:04 +0530

    Add files via upload

---
+++ maximumarray.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int arr[8] = {9,2,11,13,3,4,8,7};
+int max = -1;   //sabse chhota number denge samjo
+for (int i =0;i<=7;i++){
+    if (max<arr[i])
+    {
+        max = arr[i];
+    }
+
+    }
+    printf("%d ",max);
+
+
+    return 0;
+}
---
+++ pointersrtucture.c
@@ -0,0 +1,30 @@
+  #include<stdio.h>
+ #include<stdlib.h>
+ #include<stdbool.h>
+ typedef struct pokemon
+  {
+    int hp;
+    int speed;
+    int attack;
+    char tier;
+    char name[15];
+  }pokemon;
+ int main(){
+    pokemon pikachu;
+    pikachu.hp = 60;
+    pikachu.attack = 70;
+    pikachu.speed = 100;
+    pikachu.tier = 'A';
+    strcpy(pikachu.name,"Pikachu");
+
+    pokemon*x = &pikachu;
+    printf("%p\n",pikachu.hp);
+    printf("%p\n",pikachu.attack);
+    printf("%p\n",pikachu.speed);
+    printf("%p\n",pikachu.tier);
+    printf("%p\n",pikachu.name);
+
+    printf("%p",x);
+
+    return 0 ;
+ }
